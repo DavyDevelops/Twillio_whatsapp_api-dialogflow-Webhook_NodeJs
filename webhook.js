3abf691071b32c8496e0fa0617e8473851f2beba
Otuekong Nyong <109167028+DavyDevelops@users.noreply.github.com>
2024-02-03 02:16:52 +0000

    webhook code added

---
+++ webhook.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const dialogflow = require('@google-cloud/dialogflow');
+const twilio = require('twilio');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const sessionId = process.env.SESSION_ID;
+const projectId = process.env.PROJECT_ID;
+const languageCode = process.env.LANGUAGE_CODE;
+
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+app.post('/webhook', async (req, res) => {
+    const { Body: text, From: phoneNumber } = req.body;
+
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text,
+                languageCode,
+            },
+        },
+    };
+
+    try {
+        const responses = await sessionClient.detectIntent(request);
+        const result = responses[0].queryResult;
+        const twiml = new twilio.twiml.MessagingResponse();
+
+        twiml.message(result.fulfillmentText);
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        res.end(twiml.toString());
+    } catch (e) {
+        console.log('error:', e);
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
